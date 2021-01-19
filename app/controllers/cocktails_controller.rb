5091c2871f090e2efd359faa9fec5b4d8176ab51
jordannadroj <jordansiemens93@gmail.com>
2021-01-19 20:37:09 +0100

    added ability for user to upload photo

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,7 +24,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 
 
