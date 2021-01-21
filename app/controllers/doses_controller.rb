fe7cdeb9119a3b1adc5d6ea0150153a6f09a0546
jordannadroj <jordansiemens93@gmail.com>
2021-01-21 13:24:01 +0100

    fixed back link in new page

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,18 +1,20 @@
 class DosesController < ApplicationController
   def new
     # raise
+    # must find the cocktail which the new dose will belong to
     @cocktail = Cocktail.find(params[:cocktail_id])
+    # initialize a new dose
     @dose = Dose.new
   end
 
   def create
-    @dose = Dose.new(dose_params)
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.cocktail = @cocktail
+    @dose = Dose.new(dose_params) # create new dose with params passed into form
+    @cocktail = Cocktail.find(params[:cocktail_id]) # find the cocktail the new dose belongs to
+    @dose.cocktail = @cocktail # set the dose cocktail id the the cocktail
     if @dose.save
-      redirect_to cocktail_path(@cocktail)
+      redirect_to cocktail_path(@cocktail) # go to show page of cocktail
     else
-      render :new
+      render :new # if the form was not filled correctly, reload the form
     end
   end
 
