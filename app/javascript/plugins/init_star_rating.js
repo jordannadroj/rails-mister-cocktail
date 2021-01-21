dc68a1f8d71face5fc6722afc8ab4103b6045301
jordannadroj <jordansiemens93@gmail.com>
2021-01-21 20:39:17 +0100

    added star bar rating to review page

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -1,7 +1,7 @@
 
 // app/javascript/plugins/init_star_rating.js
 import "jquery-bar-rating";
-import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuery section)
+// import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuery section)
 
 const initStarRating = () => {
   // TODO
